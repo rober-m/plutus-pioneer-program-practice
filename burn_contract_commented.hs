2fba5f1339962e0e488ff14e9b346df05bcef45b
Robertino <48034748+llPatternll@users.noreply.github.com>
2022-01-25 07:47:02 -0800

    Commented the plutus burn contract

---
+++ burn_contract_commented.hs
@@ -0,0 +1,97 @@
+{-
+This contract is basically the same as the gift contract but with a small change.
+I'll only comment the changes, everything else, look for in the gift contract.
+-}
+
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+-- This line stops the automatic import of the Haskell standard Prelude
+-- (We use it because we import our own PlutusTx.Prelude)
+{-# LANGUAGE NoImplicitPrelude   #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TypeApplications    #-}
+{-# LANGUAGE TypeFamilies        #-}
+{-# LANGUAGE TypeOperators       #-}
+
+module Week02.Burn where
+
+import           Control.Monad       hiding (fmap)
+import           Data.Map            as Map
+import           Data.Text           (Text)
+import           Data.Void           (Void)
+import           Plutus.Contract
+import           PlutusTx            (Data (..))
+import qualified PlutusTx
+import qualified PlutusTx.Builtins   as Builtins
+import           PlutusTx.Prelude    hiding (Semigroup(..), unless)
+import           Ledger              hiding (singleton)
+import           Ledger.Constraints  as Constraints
+import qualified Ledger.Scripts      as Scripts
+import           Ledger.Ada          as Ada
+import           Playground.Contract (printJson, printSchemas, ensureKnownCurrencies, stage)
+import           Playground.TH       (mkKnownCurrencies, mkSchemaDefinitions)
+import           Playground.Types    (KnownCurrency (..))
+import           Prelude             (IO, Semigroup (..), String)
+import           Text.Printf         (printf)
+
+{-# OPTIONS_GHC -fno-warn-unused-imports #-}
+
+{-# INLINABLE mkValidator #-}
+mkValidator :: BuiltinData -> BuiltinData -> BuiltinData -> ()
+
+{-
+There are two ways to return error in Plutus:
+
+error -> This is not the standard error from Haskell. This comes from PlutusTx.Prelude. 
+            It works, but we can't see in the logs why it failed.
+traceError -> Is a variation of error to which we can provide a string that will appear in the error log.
+
+By ignoring all arguments passed to mkValidator and always returning an error, the
+validator will always fail. This will allow us to send Ada to the script, but no one
+will ever be able to retrieve them. We're, essentially, burning Ada.
+-}
+mkValidator _ _ _ = traceError "BURNT!"
+
+validator :: Validator
+validator = mkValidatorScript $$(PlutusTx.compile [|| mkValidator ||])
+
+valHash :: Ledger.ValidatorHash
+valHash = Scripts.validatorHash validator
+
+scrAddress :: Ledger.Address
+scrAddress = scriptAddress validator
+
+type GiftSchema =
+            Endpoint "give" Integer
+        .\/ Endpoint "grab" ()
+
+give :: AsContractError e => Integer -> Contract w s e ()
+give amount = do
+    let tx = mustPayToOtherScript valHash (Datum $ Builtins.mkConstr 0 []) $ Ada.lovelaceValueOf amount
+    ledgerTx <- submitTx tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String $ printf "made a gift of %d lovelace" amount
+
+grab :: forall w s e. AsContractError e => Contract w s e ()
+grab = do
+    utxos <- utxosAt scrAddress
+    let orefs   = fst <$> Map.toList utxos
+        lookups = Constraints.unspentOutputs utxos      <>
+                  Constraints.otherScript validator
+        tx :: TxConstraints Void Void
+        tx      = mconcat [mustSpendScriptOutput oref $ Redeemer $ Builtins.mkI 17 | oref <- orefs]
+    ledgerTx <- submitTxConstraintsWith @Void lookups tx
+    void $ awaitTxConfirmed $ getCardanoTxId ledgerTx
+    logInfo @String $ "collected gifts"
+
+endpoints :: Contract () GiftSchema Text ()
+endpoints = awaitPromise (give' `select` grab') >> endpoints
+  where
+    give' = endpoint @"give" give
+    grab' = endpoint @"grab" $ const grab
+
+mkSchemaDefinitions ''GiftSchema
+
+mkKnownCurrencies []
